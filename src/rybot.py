155c335a09b203e204d0163e854e8238617c3bad
Mackenzie Gray <mackenzie.gray13@ncf.edu>
2017-11-21 12:44:06 -0500

    Restructures function calling, adds dongers.
    - ᕕ( ՞ ᗜ ՞ )ᕗ

--- src/rybot.py
+++ src/rybot.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import os
+from random import choice
 import time
 from slackclient import SlackClient
 import praise
@@ -15,20 +16,9 @@ class Rybot:
         self.at_bot = '<@' + self.bot_id + '>'
         self.slack_bot_token = os.environ.get('SLACK_BOT_TOKEN')
         self.slack_client = SlackClient(self.slack_bot_token)
-        self.docs_dict = {
-            'numpy': 'https://docs.scipy.org/doc/numpy-1.13.0/index.html',
-            'scipy': 'https://docs.scipy.org/doc/scipy/reference/',
-            'pandas': 'https://pandas.pydata.org/pandas-docs/stable/api.html',
-            'mongo': 'https://docs.mongodb.com/',
-            'postgres': 'https://www.postgresql.org/docs/'
-            }
-        self.docs_undef_error = 'Sorry, I don\'t have documentation for that. Try one of {}'.format([k for k in self.docs_dict.keys()])
-        self.docs = lambda x: self.docs_dict[x] if x in self.docs_dict.keys() else self.docs_undef_error
-        self.fightme = lambda _: 'http://i.imgur.com/Skqd8Rf.jpg'
-        self.praise = lambda _: praise.praise() 
 
         # Must come after the command definitions
-        self.commands = {'docs': self.docs, 'fightme': self.fightme, 'praise': self.praise}
+        self.commands = {'docs': self.get_docs, 'fightme': self.get_donger, 'praise': self.praise}
         self.read_websocket_delay = 1  # 1 second delay between reading from firehose
 
     def start(self):
@@ -38,25 +28,26 @@ class Rybot:
         if self.slack_client.rtm_connect():
             print("Rybot connected and running!")
             while True:
-                command, channel = self.parse_slack_output(self.slack_client.rtm_read())
+                userid, command, channel = self.parse_slack_output(self.slack_client.rtm_read())
                 if command and channel:
-                    self.handle_command(command, channel)
+                    self.handle_command(userid, command, channel)
                 time.sleep(self.read_websocket_delay)
         else:
             print("Connection failed. Invalid Slack token or bot ID?")
 
-    def handle_command(self, command, channel):
+    def handle_command(self, userid, command, channel):
         '''
             Receives commands directed at the bot and determines if they
             are valid commands. If so, then acts on the commands. If not,
             returns back what it needs for clarification.
         '''
         response = None
+        username = self.userid_to_name(userid)
         tokens = command.split(' ')
         command = tokens[0]
         if command in self.commands.keys():    
             args = ' '.join(tokens[1:]).lower()
-            response = self.commands[command](args)
+            response = self.commands[command](username, args)
         if response is None:
             response = 'Try one of {}.'.format(self.commands.keys())
         self.slack_client.api_call("chat.postMessage", channel=channel,
@@ -72,9 +63,46 @@ class Rybot:
         if output_list and len(output_list) > 0:
             for output in output_list:
                 if output and 'text' in output and self.at_bot in output['text']:
-                    return output['text'].split(self.at_bot)[1].strip().lower(), output['channel']
-        return None, None
+                    return output['user'], output['text'].split(self.at_bot)[1].strip(), output['channel']
+        return None, None, None
 
+    def userid_to_name(self, userid):
+        response = self.slack_client.api_call("users.info", user=userid)
+        return response['user']['profile']['first_name']
+        
+    def praise(self, name, args):
+        '''
+        A wrapper for the praise() function which adds the user's listed first name to the praise.
+        '''
+        message = choice([
+            'You are ${{adjective}}, {0}.',
+            '${{Exclamation}} {0}! You\'re ${{adjective}}!',
+            '{0}, you are doing ${{adjective}}!'
+        ])
+        return praise.praise(message.format(name))
+
+    def get_docs(self, name, args):
+        docs_undef_error = 'Sorry, I don\'t have documentation for that.'
+        docs_dict = defaultdict(lambda: docs_undef_error)
+        docs_dict.update({
+            'numpy': 'https://docs.scipy.org/doc/numpy/',
+            'scipy': 'https://docs.scipy.org/doc/scipy/reference/',
+            'pandas': 'https://pandas.pydata.org/pandas-docs/stable/api.html',
+            'mongo': 'https://docs.mongodb.com/',
+            'postgres': 'https://www.postgresql.org/docs/'
+            })
+        return docs_dict[args]
+
+    def get_donger(self, name, args):
+        dongers = [
+            '(ᕗ ͠°  ਊ ͠° )ᕗ',
+            'ᕙ(° ͜ಠ ͜ʖ ͜ಠ°)ᓄ',
+            '(́ง◉◞౪◟◉‵)ง',
+            '(ง ° ͜ ʖ °)ง',
+            '༼ง=ಠ益ಠ=༽ง'
+        ]
+        return choice(dongers)
+    
 
 if __name__ == '__main__':
     ryb = Rybot()
