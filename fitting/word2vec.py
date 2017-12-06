7a431abe14ab068b45c7410b4cd20c32856075a3
Mackenzie Gray <mackenzie.gray13@ncf.edu>
2017-12-05 23:27:00 -0500

    word2vec works now.

--- fitting/word2vec.py
+++ fitting/word2vec.py
@@ -1,8 +1,11 @@
 import gensim
+import logging
 import numpy as np
 import pandas as pd
 import re
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                    level=logging.INFO)
 
 class Sentences:
     def __init__(self, body):
@@ -19,14 +22,20 @@ class Sentences:
         -------
         proc -- Yields a processed string to list of words
         """
-        
+
         for b in self.bodys:
-            b = re.split("<.*?>", b)
-            b = re.sub('[0-9A-z]', '', ''.join(b))
-            yield b.lower()
+            b = re.sub('([^0-9A-z ]|<.*?>)', '', b)
+            b = b.lower()
+            b = b.split(' ')
+            yield b
 
 
-q = pd.read_csv('../../stackoverflow-nlp/stacksample/Questions.csv', usecols=['Id', 'Body'], encoding='latin1')
+print("Reading in questions", flush=True)
+q = pd.read_csv('../data/Questions.csv', usecols=['Id', 'Body'], encoding='latin1')
 s = Sentences(q.Body)
 
-model = gensim.models.Word2Vec(sentences=s, min_count=12, workers=4)
+print("Beginning model fitting", flush=True)
+model = gensim.models.Word2Vec(sentences=s, min_count=1, workers=4)
+print("Finished fitting", flush=True)
+
+model.save("./word2vec_model")
